d2e92578d548e935e27e424d696c8f8b6cdceb07
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-14 10:23:58 -0600

    Finish palindromes excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,27 +1,34 @@
 const palindromes = function (pal) {
     let reverse = [];
     let arrPal = Array.from(pal.toLowerCase());
-    for(let i = (arrPal.length - 1); i >= 0; i--){
-        if (arrPal[i] == (" " || "." || "," || "!" || "?")){
+    for (let i = (arrPal.length - 1); i >= 0; i--){
+        if (arrPal[i] == " "){
             arrPal.splice(i,1);
-            i++;
-            console.log(i)
-        } else {
+        } else if (arrPal[i] == "."){
+            arrPal.splice(i,1);
+        } else if (arrPal[i] == ","){
+            arrPal.splice(i,1);
+        } else if (arrPal[i] == "!"){
+            arrPal.splice(i,1); 
+        } else 
             reverse.push(arrPal[i]);
-            console.log(reverse)
-        }
     }
+    console.log(arrPal);
+    console.log(reverse);
+    
     let m = 0;
     while (m < arrPal.length){
-        if (m === ((arrPal.length - 1)) && (arrPal[m] === reverse[m])){
+        if ((m === (arrPal.length - 1)) && (arrPal[m] === reverse[m])){
             return true;
         } else if (arrPal[m] === reverse[m]){
+            m++;
             continue;
         } else if (arrPal[m] !== reverse[m])
             return false;
-        m++;
-    } 
-};  
+    }
+    
+    
+};
 
 // Do not edit below this line
-//module.exports = palindromes;
+module.exports = palindromes;
