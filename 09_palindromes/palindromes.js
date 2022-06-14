531445a12548b2d5f583909de59b165ab1c693cb
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-14 09:31:31 -0600

    Tinker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,28 +1,27 @@
 const palindromes = function (pal) {
     let reverse = [];
     let arrPal = Array.from(pal.toLowerCase());
-    for(let i = arrPal.length; i >= 0; i--){
+    for(let i = (arrPal.length - 1); i >= 0; i--){
         if (arrPal[i] == (" " || "." || "," || "!" || "?")){
             arrPal.splice(i,1);
             i++;
+            console.log(i)
         } else {
             reverse.push(arrPal[i]);
+            console.log(reverse)
         }
     }
     let m = 0;
     while (m < arrPal.length){
-        if (m === arrPal.length){
+        if (m === ((arrPal.length - 1)) && (arrPal[m] === reverse[m])){
             return true;
         } else if (arrPal[m] === reverse[m]){
             continue;
         } else if (arrPal[m] !== reverse[m])
-            return reverse;
             return false;
         m++;
     } 
-
-    
 };  
 
 // Do not edit below this line
-module.exports = palindromes;
+//module.exports = palindromes;
